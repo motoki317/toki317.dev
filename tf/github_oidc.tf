e40b91af5eecebe1dfc1d6226bc911c92fd25b2a
motoki317 <motoki317@gmail.com>
2025-08-11 15:51:32 +0900

    Add oidc provider

---
+++ tf/github_oidc.tf
@@ -0,0 +1,58 @@
+resource "google_iam_workload_identity_pool" "github_pool" {
+  workload_identity_pool_id = "github-actions-pool"
+  display_name              = "GitHub Actions Pool"
+  description               = "Workload Identity Pool for GitHub Actions"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github_provider" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-provider"
+  display_name                       = "GitHub Provider"
+  description                        = "OIDC provider for GitHub Actions"
+  attribute_mapping = {
+    "google.subject"             = "assertion.sub"
+    "attribute.actor"            = "assertion.actor"
+    "attribute.repository"       = "assertion.repository"
+    "attribute.repository_owner" = "assertion.repository_owner"
+  }
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+  attribute_condition = "assertion.repository_owner == 'motoki317'"
+}
+
+resource "google_service_account" "github_actions" {
+  account_id   = "github-actions-tf"
+  display_name = "GitHub Actions Terraform Service Account"
+  description  = "Service account for GitHub Actions to deploy Terraform resources"
+}
+
+resource "google_project_iam_member" "github_actions_editor" {
+  project = "hobby-256508"
+  role    = "roles/editor"
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
+}
+
+resource "google_project_iam_member" "github_actions_storage_admin" {
+  project = "hobby-256508"
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
+}
+
+resource "google_service_account_iam_binding" "github_actions_workload_identity" {
+  service_account_id = google_service_account.github_actions.name
+  role               = "roles/iam.workloadIdentityUser"
+  members = [
+    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/motoki317/toki317.dev",
+  ]
+}
+
+output "workload_identity_provider" {
+  value       = google_iam_workload_identity_pool_provider.github_provider.name
+  description = "The full identifier of the Workload Identity Provider for GitHub Actions"
+}
+
+output "service_account_email" {
+  value       = google_service_account.github_actions.email
+  description = "The email of the service account to use in GitHub Actions"
+}
