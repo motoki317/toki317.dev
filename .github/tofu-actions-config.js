e06abd3f21700ad39c0705a1318090eda0f313fd
motoki317 <motoki317@gmail.com>
2025-09-25 17:55:27 +0900

    Update to v5 config

--- .github/tofu-actions-config.js
+++ .github/tofu-actions-config.js
@@ -1,3 +1,14 @@
+const config = {
+  tfBinary: 'terraform',
+  auth: {
+    mode: 'gcp-oidc',
+    gcpProject: 'hobby-256508',
+    gcpIdentityProvider: 'projects/905974313870/locations/global/workloadIdentityPools/github-actions-pool/providers/github-provider',
+  }
+}
+
 export default {
-  dirs: ['tf']
+  dirs: {
+    'tf': config,
+  }
 }
